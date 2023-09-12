515b9ff4c6b7914805119fac9ea5c10cddf8b7de
Tanooshri Vajpayee <tanooshrivajpayee06@gmail.com>
2023-09-12 15:18:02 +0530

    Final Submission

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { SearchPage } from './pages/SearchPage';
+
+function App() {
+    return (
+        <div className='bg-warning text-white '>
+            <div className='bg-dark text-white p-5'>
+            <SearchPage />
+            </div>
+           
+        </div>
+    );
+}
+
+export default App;
